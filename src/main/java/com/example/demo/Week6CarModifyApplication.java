7cbe0afbfd00a254ff370f1199dd8b3c2102037a
togoboy8 <cedrichillah@gmail.com>
2019-07-29 16:47:58 -0400

    first commit

---
+++ src/main/java/com/example/demo/Week6CarModifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6CarModifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week6CarModifyApplication.class, args);
+	}
+
+}
